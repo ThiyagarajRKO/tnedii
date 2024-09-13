671a863be76334f75d9ec220637cdba40c4b7ba7
ThiyagarajRKO <ThiyagarajRKO@gmail.com>
2024-09-14 01:17:48 +0530

    changed menu order

--- platform/core/base/resources/views/layouts/master.blade.php
+++ platform/core/base/resources/views/layouts/master.blade.php
@@ -14,6 +14,12 @@
                         <div class="sidebar-content">
                             @include('core/base::layouts.partials.sidebar-search')
                             <ul class="page-sidebar-menu page-header-fixed page-sidebar-menu-closed" data-keep-expanded="false" data-auto-scroll="true" data-slide-speed="200">
+                                  <li class="nav-item">
+                                    <a href="{{ url('/training/applicants') }}" class="nav-link nav-toggle">
+                                        <i class="fa fa-users"></i>
+                                        <span class="title">Training Applicants</span>
+                                    </a>
+                                </li>
                                 {{-- @Customized Ramesh Esakki - Start --}}
                                     @if(!is_plugin_active('backend-menu') || (auth() && auth()->user() && auth()->user()->manage_supers))
                                         @include('core/base::layouts.partials.sidebar')
@@ -25,12 +31,6 @@
                                         !!}
                                     @endif
                                 {{-- @Customized Ramesh Esakki - End --}}
-                                <li class="nav-item">
-                                    <a href="{{ url('/training/applicants') }}" class="nav-link nav-toggle">
-                                        <i class="fa fa-users"></i>
-                                        <span class="title">Training Applicants</span>
-                                    </a>
-                                </li>
                             </ul>
                         </div>
                     </div>
